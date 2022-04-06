507d2d528d01f511c866cf1785839138100bfe49
Pedro Sanchez <pedro.sanchez@infoprice.co>
2022-04-06 00:49:15 -0300

    Servidor,index,post, e js

---
+++ servidor.py
@@ -0,0 +1,71 @@
+import socket
+import json
+
+REQUESTS = {
+    "GET": "/index.html", 
+    "POST": "/post.html", 
+    "PUT": "/put.html", 
+    "DELETE": "/post.html"
+    }
+
+message=''
+serverPort = 8080
+
+#cria o socket
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#atribuir o socket a uma porta especifica
+serverSocket.bind(("", serverPort))
+
+#inicia o "listening"
+serverSocket.listen(1)
+
+print("Servidor HTTP/1.1 Inicializado")
+
+while True:
+    connectionSocket, addr = serverSocket.accept()
+    print("Cliente {} conectado ao servidor".format(addr))
+    request = connectionSocket.recv(1024).decode()
+
+    split_request = request.split()
+    
+    if split_request[0] == 'GET':
+        
+        if split_request[1] == '/' or split_request[1] == REQUESTS['GET']:
+            fin = open("web" + REQUESTS['GET'])
+            content = fin.read()
+            fin.close()
+            response = "HTTP/1.1 200 OK\n\n" + content
+        elif split_request[1] == REQUESTS['POST']:
+            fin = open("web" + REQUESTS['POST'])
+            content = fin.read()
+            fin.close()
+            response = "HTTP/1.1 200 OK\n\n" + content
+        elif split_request[1] == REQUESTS['PUT']:
+            fin = open("web" + REQUESTS['PUT'])
+            content = fin.read()
+            fin.close()
+            response = "HTTP/1.1 200 OK\n\n" + content
+        elif split_request[1] == REQUESTS['DELETE']:
+            fin = open("web" + REQUESTS['DELETE'])
+            content = fin.read()
+            fin.close()
+            response = "HTTP/1.1 200 OK\n\n" + content
+
+    elif split_request[0] == 'POST':
+        
+        response = "HTTP/1.1 200 OK\n\n" + content
+    elif split_request[0] == 'PUT':
+        
+        response = "HTTP/1.1 200 OK\n\n" + content
+    elif split_request[0] == 'DELETE':
+        
+        response = "HTTP/1.1 200 OK\n\n" + content
+    else:
+        print("Comando não pode ser interpretado por esse servidor!")
+        response = ("ERRO! Servidor não reconhece esse comando!").encode()
+        connectionSocket.send(response)
+
+    connectionSocket.sendall(response.encode())
+
+    connectionSocket.close()
---
+++ web/main.js
@@ -0,0 +1,13 @@
+
+// function fazPost(url){
+//     let resquest = new XMLHttpRequest()
+//     resquest.open("POST", url, false)
+//     resquest.send()
+//     return resquest.responseText
+// }
+
+// function main(){
+//     produtos = fazPost("http://localhost:8080/post")
+// }
+
+// main()
